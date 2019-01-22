d8a8d43842478e4dd05e55fdee1305f230c3224c
wlccomeon <wlccomeon@163.com>
2019-01-22 16:01:00 +0800

    添加使用mysql插件动态生成的dao、po和mapper文件

---
+++ src/main/java/com/lcmall/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package com.lcmall.dao;
+
+import com.lcmall.po.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
---
+++ src/main/java/com/lcmall/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.lcmall.dao;
+
+import com.lcmall.po.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
